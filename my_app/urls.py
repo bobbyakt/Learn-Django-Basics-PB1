35e7c840b17c4960f88fe94c789e5d92819c673f
bobbyakt <bobbyankit04@gmail.com>
2023-04-22 20:28:08 +0530

    .gitignore is now working

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from my_app.views import hello_world
+from my_app.views import hello_world,hello_temp
 
 urlpatterns = [
     path('hello', hello_world),
+    path('hello_temp', hello_temp)
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 def hello_world(request):
     html = "<html><body>you are hitting the hello world view ! </body></html>" 
     return HttpResponse(html)
+
+def hello_temp(request):
+    return render(request,"hello_temp.html")
